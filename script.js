ba0f0617294b79fc8bf2d14e6b16b8dd55c97bd0
Lawal Ameh <amehlawal@gmail.com>
2023-02-19 15:58:40 +0100

    created the unordered list and rendered myLeads in the unordered list

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let myLeads = []
 const inputEl = document.getElementById('input-el')
-
+const ulEl = document.getElementById('ul-el')
 
 const inputBtn = document.getElementById('input-btn')
 inputBtn.addEventListener('click', function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for(let i = 0; i < myLeads.length; i++){
+    ulEl.textContent += myLeads[i] + '\n';
+}
