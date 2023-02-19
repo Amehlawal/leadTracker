124aecd73c556916cac3c7c4a627b5c0b0027712
Lawal Ameh <amehlawal@gmail.com>
2023-02-20 00:28:35 +0100

    created the render function

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let myLeads = ['www.tomorrow.com', 'www.topa.com']
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
 const inputBtn = document.getElementById('input-btn')
 inputBtn.addEventListener('click', function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
-let listItems = ""
 
-for(let i = 0; i < myLeads.length; i++){
+function renderLeads(){
+    let listItems = ""
+
+    for(let i = 0; i < myLeads.length; i++){
         listItems += '<li>' +myLeads[i] +'</li>'
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
