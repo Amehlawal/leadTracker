57ff0accfc82e614408e7a9d8d3a2523ecf101e4
Lawal Ameh <amehlawal@gmail.com>
2023-02-19 15:39:35 +0100

    pushed values from my input fields to my leads array

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const inputEl = document.getElementById('input-el')
 
 const inputBtn = document.getElementById('input-btn')
 inputBtn.addEventListener('click', function(){
-    console.log('button clicked')
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
