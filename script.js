e2b8fc7f4b6eb9fdd350ce6c0c672af6c32e0592
Lawal Ameh <amehlawal@gmail.com>
2023-02-20 01:23:11 +0100

    made use of template strings

--- script.js
+++ script.js
@@ -13,7 +13,13 @@ function renderLeads(){
     let listItems = ""
 
     for(let i = 0; i < myLeads.length; i++){
-        listItems += "<li><a target='_blank_' href='"+ myLeads[i]+ "'>" +myLeads[i] +"</a></li>"
+        //listItems += "<li><a target='_blank_' href='"+ myLeads[i]+ "'>" +myLeads[i] +"</a></li>"
+        listItems += `
+        <li>
+            <a target='_blank_' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`
     }
     ulEl.innerHTML = listItems
 }
