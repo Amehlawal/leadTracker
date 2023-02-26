ed393f5a5d42f04cdbcd38672e6882a6e4f593f3
Lawal Ameh <amehlawal@gmail.com>
2023-02-26 05:03:42 +0100

    checking local storage before rendering

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const ulEl = document.getElementById('ul-el')
 const inputBtn = document.getElementById('input-btn')
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener('click', function(){
     myLeads.push(inputEl.value)
